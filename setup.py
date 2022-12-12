a932bebffc517eac8cfd6f775c0ca2f2c595840b
rahulkumar <rsk.150896@gmail.com>
2022-12-12 10:01:52 +0530

    updated the setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import find_packages, setup
+from typing import List
 
-def get_requirements():
-    pass
+#listing down all the installable library names from requirements.txt file to requirement_list 
+REQUIREMENT_FILE_NAME='rquirements.txt'
+def get_requirements()->List[str]:...
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list] #in order to change line, '\n' is invisbly present in the requirements.txt file at the end of each line, we want to remove that
+
+    if HYPHEN_E_DOT in requirement_list: #-e . is not a library, we don't want to include it in our list of libraries
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
 
 setup(
     name = 'sensor',
