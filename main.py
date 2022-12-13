b3ac2a6b008870ee3840a1dbd2e072586f70e8a2
rahulkumar <rahulxyz96@gmail.com>
2022-12-13 15:04:21 +0530

    updated

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import sys, os
 from sensor.utils import get_collection_as_dataframe
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity import config_entity
+from sensor.components import data_ingestion
+from sensor.components.data_ingestion import DataIngestion
 
 if __name__=="__main__":
      try:
           training_pipeline_config = config_entity.TrainingPipelineConfig()
           data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -19,15 +19,15 @@ class DataIngestionConfig:
         try:
             self.database_name="aps"
             self.collection_name="sensor"
-            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion") #data_ingestion_dir folder will get created in each of the timestamps directory
-            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
-            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
-            self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion") #data_ingestion folder will get created in each of the timestamps directory
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME) #feature_store is the directory name inside data_ingestion directory inside which sensor.csv file will be saved
+            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME) #dataset is the directory name inside data_ingestion directory inside which train.csv file will be saved
+            self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME) #dataset is the directory name inside data_ingestion directory inside which test.csv file will be saved
             self.test_size = 0.2
         except Exception  as e:
             raise SensorException(e,sys)     
     
-    def to_dict(self,)->dict:   #we want all the outputs in the JSON format which is nothing but dictionary in order to store them in mongodb, dataset will be called from mongodb & converted into dataframe after which dataset is ready for train-test split
+    def to_dict(self,)->dict:   #eg: {'database_name': 'aps', 'collection_name': 'sensor', 'data_ingestion_dir': '/config/workspace/artifact/12132022__142058/data_ingestion'}
         try:
             return self.__dict__
         except Exception  as e:
